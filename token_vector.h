ff341cc69f17dde578780cb61f6d1c6ad63efea9
Michael Gerhold <michael.gerhold@web.de>
2022-04-10 14:18:17 +0200

    created macros to define dynamic arrays (vectors)

--- token_vector.h
+++ token_vector.h
@@ -1,12 +1,6 @@
 #pragma once
 
 #include "token.h"
+#include "vector.h"
 
-typedef struct {
-    Token* data;
-    size_t size, capacity;
-} TokenVector;
-
-TokenVector token_vector_create(void);
-void token_vector_push(TokenVector* vector, Token token);
-void token_vector_free(TokenVector* vector);
+CREATE_VECTOR_DECLARATION(TokenVector, Token, token_vector)
---
+++ vector.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define CREATE_VECTOR_DECLARATION(name, element_type, prefix) \
+typedef struct { \
+    element_type * data; \
+    size_t size, capacity; \
+} name; \
+ \
+name prefix##_create(void); \
+void prefix##_push(name* vector, element_type element); \
+void prefix##_free(name* vector);
+
+#define CREATE_VECTOR_DEFINITION(name, element_type, prefix) \
+ \
+name prefix##_create(void) { \
+    return (name){ \
+        .data = NULL, \
+        .size = 0, \
+        .capacity = 0, \
+    }; \
+} \
+ \
+static void grow(name* const vector) { \
+    vector->capacity = vector->capacity == 0 ? 1 : vector->capacity * 2; \
+    element_type* new_data = realloc(vector->data, sizeof(element_type) * vector->capacity); \
+    if (!new_data) { \
+        fprintf(stderr, "Unable to allocate memory\n"); \
+        free(vector->data); \
+        exit(EXIT_FAILURE); \
+    } \
+    vector->data = new_data; \
+} \
+ \
+void prefix##_push(name* const vector, element_type element_type) { \
+    bool const needs_to_grow = vector->size == vector->capacity; \
+    if (needs_to_grow) { \
+        grow(vector); \
+    } \
+    vector->data[vector->size] = element_type; \
+    ++vector->size; \
+} \
+ \
+void prefix##_free(name* const vector) { \
+    free(vector->data); \
+    vector->size = 0; \
+    vector->capacity = 0; \
+}
